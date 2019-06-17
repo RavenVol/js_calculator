ee9fa3add41b85366b4a8866e12a7fdae7170f27
RavenVol <r.volianiuk@gmail.com>
2019-06-17 21:20:59 +0300

    Supervisor's corrections are applied

--- calculator.js
+++ calculator.js
@@ -2,33 +2,29 @@ function calculateMe(form) {
     let firstDigit = form.firstNumber.value;
     let secondDigit = form.secondNumber.value;
     const operator = form.operator.value;
-    let result = "";
+    let result = '';
 
-    isNaN(firstDigit) ? 
-        result = result + "!!! WRONG DATA TYPE WARNING !!! \n Number must be entered on the first position.\n\n"
-        :
-        firstDigit = Number(firstDigit);
+    isNaN(firstDigit) 
+        ? result = result + '!!! WRONG DATA TYPE WARNING !!! \n Number must be entered on the first position.\n\n'
+        : firstDigit = Number(firstDigit);
     
-    isNaN(secondDigit) ? 
-        result = result + "!!! WRONG DATA TYPE WARNING !!! \n Number must be entered on the second position.\n\n"
-        :
-        secondDigit = Number(secondDigit);
+    isNaN(secondDigit) 
+        ? result = result + '!!! WRONG DATA TYPE WARNING !!! \n Number must be entered on the second position.\n\n'
+        : secondDigit = Number(secondDigit);
     
     operator != '+' 
         && operator != '-' 
         && operator != '*' 
-        && operator != '/' ? 
-        result = result + "!!! OPERATOR MISMATCH WARNING !!!\n" + operator + " has a wrong data format.\n\n"
-        :
-        null;
+        && operator != '/'
+        ? result = result + '!!! OPERATOR MISMATCH WARNING !!!\n' + operator + ' has a wrong data format.\n\n'
+        : null;
 
-    operator == '/' && secondDigit == 0 ? 
-        result = result + "!!! ERROR !!!\n Devision by zero!" 
-        : 
-        null;
+    operator == '/' && secondDigit == 0 
+        ? result = result + '!!! ERROR !!!\n Devision by zero!' 
+        : null;
 
     if (result) {
-        document.getElementById("result").textContent = 'ERROR';
+        document.getElementById('result').textContent = 'ERROR';
         alert(result);
     } else {
         switch (operator) {
@@ -45,6 +41,6 @@ function calculateMe(form) {
                 result = firstDigit / secondDigit;
                 break;
         }
-        document.getElementById("result").textContent = result;
+        document.getElementById('result').textContent = result;
     }
 }
